df3ff91d3adf9b6ccc23cb32926bce02bfd365ef
Aura's Laptop <edvin.garibovic@gmail.com>
2024-05-24 17:23:56 +0200

    cleaned up client code

--- chatClient.py
+++ chatClient.py
@@ -13,8 +13,8 @@ HEADER_LENGTH = 2
 
 CERTIFIKATI = "certificates/"
 SERVER_KEY = CERTIFIKATI + "server.crt"
-private_key = "privateJanez"
-IME = "janez"
+private_key = ""
+IME = ""
 
 def init():
     global IME, private_key
@@ -29,7 +29,7 @@ def init():
     try:
         decision = int(choice)
         IME, private_key = choose_user[decision]
-    except Exception as e:
+    except Exception:
         decision = 0
         IME, private_key = choose_user[decision]
 
@@ -68,40 +68,30 @@ def receive_message(your_sock):
 
     message = None
     if message_length > 0:  # ce je vse OK
-        # print(f"Message recieved! message length: {message_length}\n")
-        # print(struct.unpack("!H", header))
         message = receive_fixed_length_msg(
             your_sock, message_length
         )  # preberi sporocilo
         message = message.decode("utf-8")
 
-    return json.loads(message)
+    return json.loads(message)  # Iz JSON tipa naredi slovar
 
 def send_message(your_sock, message):
-    # This is where I will try replacing the string with a json string
     current_time = datetime.now().strftime("@%H-%M ")
 
     mes_dict = dict(message=message)
     mes_dict["time"] = current_time
     mes_dict["user"] = ""
-    # print(json.dumps(mes_dict))
-
     message = json.dumps(mes_dict)
 
-    # message = current_time + message
-    # message = datetime.now().strftime('%H-%M') + " " + message
-
-    encoded_message = message.encode(
-        "utf-8"
-    )  # pretvori sporocilo v niz bajtov, uporabi UTF-8 kodno tabelo
+    # pretvori sporocilo v niz bajtov, uporabi UTF-8 kodno tabelo
+    encoded_message = message.encode("utf-8")
 
     # ustvari glavo v prvih 2 bytih je dolzina sporocila (HEADER_LENGTH)
     # metoda pack "!H" : !=network byte order, H=unsigned short
     header = struct.pack("!H", len(encoded_message))
 
-    message = (
-            header + encoded_message
-    )  # najprj posljemo dolzino sporocilo, slee nato sporocilo samo
+    message = (header + encoded_message)
+    # najprj posljemo dolzino sporocilo, slee nato sporocilo samo
     your_sock.sendall(message)
 
 # message_receiver funkcija tece v loceni niti
@@ -110,25 +100,12 @@ def message_receiver():
 
     while True:
         msg_received = receive_message(sock)
-
-        # msg_time = msg_received[1:msg_received.index("FROM:") - 1]
-        # user_from = msg_received[msg_received.index("FROM:") + len("FROM:"):msg_received.find(";")]
-
-        # msg_from = msg_time + " [" + user_from + "]: "
-        # msg_actual = msg_received[msg_received.find(";") + 1:]
-
-        # print("raw: ", end="")
-        # print(msg_received)
-
         msg_from = msg_received["user"]
         msg_time = msg_received["time"]
         msg_actual = msg_received["message"]
-        # print("actual: ", end="")
-        # print(msg_actual)
 
         if len(msg_received) > 0:  # ce obstaja sporocilo
             print(msg_time + " <" + msg_from + ">: " + msg_actual)
-            # print(msg_from + msg_actual)  # izpisi
 
 # povezi se na streznik
 print("[system] connecting to chat server ...")
@@ -137,8 +114,6 @@ my_ssl_ctx = setup_SSL_context()
 sock = my_ssl_ctx.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
 sock.connect((SERVER_IP, PORT))
 print("[system] connected!")
-
-
 
 # zazeni message_receiver funkcijo v loceni niti
 thread = threading.Thread(target=message_receiver)
